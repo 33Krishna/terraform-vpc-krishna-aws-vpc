0daad970ae02ef08e7c7be4dd74ce312fa64d376
Rati Krishna Moharana <mratikrishna@gmail.com>
2026-04-01 12:53:46 +0530

    Add files via upload

---
+++ module/vpc/variables.tf
@@ -0,0 +1,26 @@
+variable "vpc_config" {
+  description = "To get the CIDR and Name of VPC from user"
+  type = object({
+    cidr_block = string
+    name       = string
+  })
+  validation {
+    condition     = can(cidrnetmask(var.vpc_config.cidr_block))
+    error_message = "Invalid CIDR Format - ${var.vpc_config.cidr_block} should be in format like 10.0.0.0/16"
+  }
+}
+
+variable "subnet_config" {
+  # sub1={cidr=.. az=..} sub2={same..} sub3={same..} -> This is format of subnet
+  description = "Get the CIDR and AZ for the subnets"
+  type = map(object({
+    cidr_block = string
+    az         = string
+    public     = optional(bool, false)
+  }))
+  validation {
+    # validation should be in [true, true, false/true]
+    condition     = alltrue([for config in var.subnet_config : can(cidrnetmask(config.cidr_block))])
+    error_message = "Invalid CIDR Format"
+  }
+}
